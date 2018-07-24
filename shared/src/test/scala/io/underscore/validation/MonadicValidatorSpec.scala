d999aeae4139a8ad444d2b7758073bffe5443da8
Raúl Piaggio <rpiaggio@gmail.com>
2018-07-24 18:25:09 -0300

    More tests.

--- shared/src/test/scala/io/underscore/validation/MonadicValidatorSpec.scala
+++ shared/src/test/scala/io/underscore/validation/MonadicValidatorSpec.scala
@@ -10,13 +10,20 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 class MonadicValidatorSpec extends Specification {
 
   val db = HashSet("raul@example.com", "contributor@example.com")
-  def existsInDB(email: String): Future[Boolean] = Future.successful(db.contains(email))
+  def existsInDBFuture(email: String): Future[Boolean] = Future.successful(db.contains(email))
 
   val isEmail = matchesRegex("^[^@]+@[^@]+$".r, "Must be an email")
+  def isUnique(implicit ec: ExecutionContext) = validate[String]("Email is already registered") { email => existsInDBFuture(email).map(!_) }
 
   "Future unique email" >> { implicit ec: ExecutionContext =>
-    val isUnique = validate[String]("Email is already registered") { email => existsInDB(email).map(!_) }
     val validator = isEmail and isUnique
+    Await.result(validator("dave@example.com"), Duration.Inf) mustEqual pass
+    Await.result(validator("raul@example.com"), Duration.Inf) mustEqual fail("Email is already registered")
+    Await.result(validator("raul@"), Duration.Inf)            mustEqual fail("Must be an email")
+  }
+
+  "Future unique email (validator reversed)" >> { implicit ec: ExecutionContext =>
+    val validator = isUnique and isEmail
     Await.result(validator("dave@example.com"), Duration.Inf) mustEqual pass
     Await.result(validator("raul@example.com"), Duration.Inf) mustEqual fail("Email is already registered")
     Await.result(validator("raul@"), Duration.Inf)            mustEqual fail("Must be an email")
